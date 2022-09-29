384d51308ff33697d8b24f6ea8fb0513b7ac03c6
cbkremer <cbpontokremer@gmail.com>
2022-09-29 17:36:53 -0300

    fazendo paginação do usuario

--- src/main/java/com/stefanini/spotify/security/SecurityConfiguration.java
+++ src/main/java/com/stefanini/spotify/security/SecurityConfiguration.java
@@ -1,10 +1,28 @@
 package com.stefanini.spotify.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+    //autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+    //configuração de recursos estaticos js, css, imagens etc
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
 }
